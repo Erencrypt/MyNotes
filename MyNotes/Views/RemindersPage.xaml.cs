dd7f940bc526307f61dd0d2cb4d8e3a0f86d13c8
ErenCanUtku <erencan.utku52@gmail.com>
2023-05-25 20:32:13 +0300

    reminders page design complated
    list view item designs has been made and some examples added for populating list view and testing it.

--- MyNotes/Views/RemindersPage.xaml.cs
+++ MyNotes/Views/RemindersPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 
 using MyNotes.ViewModels;
+using Windows.System;
 
 namespace MyNotes.Views;
 
@@ -15,5 +16,19 @@ public sealed partial class RemindersPage : Page
     {
         ViewModel = App.GetService<RemindersViewModel>();
         InitializeComponent();
+        List<Reminder> items = new List<Reminder>();
+        for (int i = 0; i < 4; i++)
+        {
+            items.Add(new Reminder() { ReminderHeader = "test Reminder 1", ReminderText = "this is the thing i want to remember" });
+            items.Add(new Reminder() { ReminderHeader = "reminder test", ReminderText = "this is another thing to remember" });
+            items.Add(new Reminder() { ReminderHeader = "test 123", ReminderText = "this one... im not sure" });
+        }
+        LstReminders.ItemsSource = items;
+    }
+    public class Reminder
+    {
+        public string ReminderHeader { get; set; }
+
+        public string ReminderText { get; set; }
     }
 }
