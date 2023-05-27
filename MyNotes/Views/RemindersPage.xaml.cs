58ceac42768198e30c086326d3a0fe4ecf6443c4
ErenCanUtku <erencan.utku52@gmail.com>
2023-05-27 03:06:57 +0300

    reminder list visuals changed and data types added

--- MyNotes/Views/RemindersPage.xaml.cs
+++ MyNotes/Views/RemindersPage.xaml.cs
@@ -19,9 +19,9 @@ public sealed partial class RemindersPage : Page
         List<Reminder> items = new List<Reminder>();
         for (int i = 0; i < 4; i++)
         {
-            items.Add(new Reminder() { ReminderHeader = "test Reminder 1", ReminderText = "this is the thing i want to remember" });
-            items.Add(new Reminder() { ReminderHeader = "reminder test", ReminderText = "this is another thing to remember" });
-            items.Add(new Reminder() { ReminderHeader = "test 123", ReminderText = "this one... im not sure" });
+            items.Add(new Reminder() { ReminderHeader = "test Reminder 1", ReminderText = "this is the thing i want to remember", Date=DateTime.Now, Repeat="true" });
+            items.Add(new Reminder() { ReminderHeader = "reminder test", ReminderText = "this is another thing to remember", Date = DateTime.Now, Repeat = "false" });
+            items.Add(new Reminder() { ReminderHeader = "test 123", ReminderText = "this one... im not sure", Date = DateTime.Now, Repeat = "false" });
         }
         LstReminders.ItemsSource = items;
     }
@@ -30,5 +30,8 @@ public sealed partial class RemindersPage : Page
         public string ?ReminderHeader { get; set; }
 
         public string ?ReminderText { get; set; }
+
+        public DateTime? Date { get; set; }
+        public string? Repeat { get; set; }
     }
 }
