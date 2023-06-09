27e19aa63df73bb68acd18ac6cbee4795c41d28d
ErenCanUtku <erencan.utku52@gmail.com>
2023-06-09 23:35:15 +0300

    Override added for reminders

--- MyNotes/Helpers/MoveFile.cs
+++ MyNotes/Helpers/MoveFile.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml;
 using Windows.Storage;
+using System.Collections.ObjectModel;
+using static MyNotes.Views.RemindersPage;
 
 namespace MyNotes.Helpers
 {
@@ -14,7 +16,6 @@ namespace MyNotes.Helpers
                 var selectedItem = list.SelectedItem;
                 if (selectedItem != null)
                 {
-
                     var directory = notesFolder.Path.ToString() + "\\"+from+"\\" + list.SelectedItem.ToString() + ".rtf";
                     var dir = notesFolder.Path.ToString() + "\\" + to + "\\";
                     var folder = await StorageFolder.GetFolderFromPathAsync(dir);
@@ -25,14 +26,43 @@ namespace MyNotes.Helpers
                 }
                 else
                 {
-                    ContentDialog noWifiDialog = new ContentDialog()
+                    ContentDialog noWifiDialog = new()
                     { XamlRoot = root, Title = "Info".GetLocalized(), Content = "NoSelection".GetLocalized(), CloseButtonText = "Ok".GetLocalized() };
                     await noWifiDialog.ShowAsync();
                 }
             }
             catch (Exception ex)
             {
-                ContentDialog noWifiDialog = new ContentDialog()
+                ContentDialog noWifiDialog = new()
+                { XamlRoot = root, Title = "Error".GetLocalized(), Content = "Error_Meesage2".GetLocalized() + ex.Message, CloseButtonText = "Ok".GetLocalized() };
+                await noWifiDialog.ShowAsync();
+            }
+        }
+        public async void Move(string from, string to, ListView list, XamlRoot root, Reminder reminder, ObservableCollection<Reminder> items)
+        {
+            try
+            {
+                var selectedItem = list.SelectedItem;
+                if (selectedItem != null)
+                {
+                    var directory = notesFolder.Path.ToString() + "\\" + from + "\\" + reminder.ReminderHeader.ToString() + ".txt";
+                    var dir = notesFolder.Path.ToString() + "\\" + to + "\\";
+                    var folder = await StorageFolder.GetFolderFromPathAsync(dir);
+                    var file = await StorageFile.GetFileFromPathAsync(directory);
+                    await file.CopyAsync(folder, reminder.ReminderHeader.ToString() + ".txt", NameCollisionOption.GenerateUniqueName);
+                    await file.DeleteAsync();
+                    items.Remove(reminder);
+                }
+                else
+                {
+                    ContentDialog noWifiDialog = new()
+                    { XamlRoot = root, Title = "Info".GetLocalized(), Content = "NoSelection".GetLocalized(), CloseButtonText = "Ok".GetLocalized() };
+                    await noWifiDialog.ShowAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                ContentDialog noWifiDialog = new()
                 { XamlRoot = root, Title = "Error".GetLocalized(), Content = "Error_Meesage2".GetLocalized() + ex.Message, CloseButtonText = "Ok".GetLocalized() };
                 await noWifiDialog.ShowAsync();
             }
