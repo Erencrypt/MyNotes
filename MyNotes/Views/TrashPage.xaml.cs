9e2fd739f5bf3af69d74be03370bb2cd5d62df85
ErenCanUtku <erencan.utku52@gmail.com>
2023-06-12 19:17:48 +0300

    Reminder list added

--- MyNotes/Views/TrashPage.xaml.cs
+++ MyNotes/Views/TrashPage.xaml.cs
@@ -3,20 +3,40 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using MyNotes.Helpers;
 using MyNotes.ViewModels;
+using MyNotes.Models;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using System.Text;
 using Windows.Storage;
+using static MyNotes.Views.RemindersPage;
 
 namespace MyNotes.Views;
 
 public sealed partial class TrashPage : Page
 {
     private readonly StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+    private ObservableCollection<Reminder> items = new();
     public TrashViewModel ViewModel
-    { get; }
+    {
+        get;
+    }
     public TrashPage()
     {
         ViewModel = App.GetService<TrashViewModel>();
         InitializeComponent();
-        ListFiles();
+        //Localizatinos
+        deleteFlyoutNoteText.Text = "DeleteFlyout2".GetLocalized();
+        deleteNoteFly.Content = "DeleteConfirm".GetLocalized();
+        ToolTipService.SetToolTip(deleteNote, "Delete".GetLocalized());
+        ToolTipService.SetToolTip(restoreNote, "Restore".GetLocalized());
+        deleteFlyoutReminderText.Text = "DeleteFlyout2".GetLocalized();
+        deleteReminderFly.Content = "DeleteConfirm".GetLocalized();
+        ToolTipService.SetToolTip(deleteReminder, "Delete".GetLocalized());
+        ToolTipService.SetToolTip(restoreReminder, "Restore".GetLocalized());
+
+        LstReminders.ItemsSource = items;
+        ListNotes();
+        ListReminders();
         if (LstNotes.Items.Count < 1)
         {
             EmptyText.Visibility = Visibility.Visible;
@@ -25,12 +45,16 @@ public sealed partial class TrashPage : Page
         {
             EmptyText.Visibility = Visibility.Collapsed;
         }
-        deleteFlyout.Text = "DeleteFlyout2".GetLocalized();
-        deleteNoteFly.Content = "DeleteNote_Button".GetLocalized();
-        ToolTipService.SetToolTip(deleteNote, "DeleteNote".GetLocalized());
-        ToolTipService.SetToolTip(restoreNote, "RestoreNote".GetLocalized());
+        if (LstReminders.Items.Count < 1)
+        {
+            EmptyText.Visibility = Visibility.Visible;
+        }
+        else
+        {
+            EmptyText.Visibility = Visibility.Collapsed;
+        }
     }
-    private void ListFiles()
+    private void ListNotes()
     {
         DirectoryInfo dinfo = new(storageFolder.Path.ToString() + "\\Trash");
         FileInfo[] Files = dinfo.GetFiles("*.rtf");
@@ -40,6 +64,38 @@ public sealed partial class TrashPage : Page
         {
             LstNotes.Items.Add(file.Name[..^4]);
         }
+    }
+    private void ListReminders()
+    {
+        DirectoryInfo dinfo = new(storageFolder.Path.ToString() + "\\Trash");
+        FileInfo[] Files = dinfo.GetFiles("*.txt");
+        List<FileInfo> orderedList = Files.OrderByDescending(x => x.CreationTime).ToList();
+        string fullPath;
+        items.Clear();
+        foreach (FileInfo file in orderedList)
+        {
+            fullPath = dinfo.ToString() + "\\" + file.Name;
+            string readText = File.ReadAllText(fullPath, Encoding.UTF8);
+            string[] lines = readText.Split("\r\n");
+            Regex regex = MyRegex();
+            string[] s;
+            DateTime t;
+            string[] tt;
+            if (lines.Length == 3)
+            {
+                t = Convert.ToDateTime(lines[2]);
+                s = regex.Split(t.ToString());
+                items.Add(new Reminder() { ReminderHeader = file.Name[..^4], ReminderText = lines[1], DateTime = s[1][..^3] + " " + s[2], Repeat = lines[0] });
+            }
+            else if (lines.Length == 4)
+            {
+                t = Convert.ToDateTime(lines[3] + " " + lines[2]);
+                tt = regex.Split(t.ToString());
+                items.Add(new Reminder() { ReminderHeader = file.Name[..^4], ReminderText = lines[1], DateTime = tt[0] + " " + tt[1][..^3] + " " + tt[2], Repeat = lines[0] });
+            }
+        }
+        LstReminders.ItemsSource = items;
+        LstReminders.UpdateLayout();
     }
     private async void DeleteNote()
     {
@@ -72,16 +128,44 @@ public sealed partial class TrashPage : Page
             await noWifiDialog.ShowAsync();
         }
     }
-    private void DeleteNote_Click(object sender, RoutedEventArgs e)
+    private async void DeleteReminder()
     {
-        if (LstNotes.Items.Count <= 1)
+        try
         {
-            EmptyText.Visibility = Visibility.Visible;
+            var selectedItem = LstReminders.SelectedItem;
+            if (selectedItem != null)
+            {
+                var directory = storageFolder.Path.ToString() + @"\Trash\" + LstReminders.SelectedItem.ToString() + ".txt";
+                var file = await StorageFile.GetFileFromPathAsync(directory);
+                await file.DeleteAsync();
+                LstReminders.Items.Remove(selectedItem);
+            }
+            else
+            {
+                ContentDialog noWifiDialog = new()
+                { XamlRoot = XamlRoot, Title = "Info".GetLocalized(), Content = "NoSelection".GetLocalized(), CloseButtonText = "Ok".GetLocalized() };
+                await noWifiDialog.ShowAsync();
+            }
         }
-        DeleteNote();
-        deleteNote.Flyout.Hide();
+        catch (Exception ex)
+        {
+            ContentDialog noWifiDialog = new()
+            {
+                XamlRoot = XamlRoot,
+                Title = "Error".GetLocalized(),
+                Content = "Error_Meesage2".GetLocalized() + ex.Message,
+                CloseButtonText = "Ok".GetLocalized()
+            };
+            await noWifiDialog.ShowAsync();
+        }
     }
-
+    private void LstNotes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (!deleteNote.IsEnabled)
+        {
+            deleteNote.IsEnabled = true;
+        }
+    }
     private void RestoreNote_Click(object sender, RoutedEventArgs e)
     {
         if (LstNotes.Items.Count <= 1)
@@ -91,12 +175,45 @@ public sealed partial class TrashPage : Page
         MoveFile moveFile = new();
         moveFile.Move("Trash", "Notes", LstNotes, XamlRoot);
     }
-
-    private void LstNotes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    private void DeleteNote_Click(object sender, RoutedEventArgs e)
+    {
+        if (LstNotes.Items.Count <= 1)
+        {
+            EmptyText.Visibility = Visibility.Visible;
+        }
+        DeleteNote();
+        deleteNote.Flyout.Hide();
+    }
+    private void LstReminders_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         if (!deleteNote.IsEnabled)
         {
-            deleteNote.IsEnabled = true;
+            deleteReminder.IsEnabled = true;
         }
     }
+    private void RestoreReminder_Click(object sender, RoutedEventArgs e)
+    {
+        if (LstReminders.Items.Count <= 1)
+        {
+            EmptyText2.Visibility = Visibility.Visible;
+        }
+        if (LstReminders.SelectedItem != null)
+        {
+            Reminder? rm = LstReminders.SelectedItem as Reminder;
+            MoveFile moveFile = new();
+            moveFile.Move("Trash", "Reminders", LstReminders, XamlRoot, rm!, items);
+        }
+        
+    }
+    private void DeleteReminder_Click(object sender, RoutedEventArgs e)
+    {
+        if (LstReminders.Items.Count <= 1)
+        {
+            EmptyText2.Visibility = Visibility.Visible;
+        }
+        DeleteReminder();
+        deleteReminder.Flyout.Hide();
+    }
+    [GeneratedRegex("\\s")]
+    private static partial Regex MyRegex();
 }
