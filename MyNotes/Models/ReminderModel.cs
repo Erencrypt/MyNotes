35b06140d13564c3f0485850ca2be2244f893f5d
ErenCanUtku <erencan.utku52@gmail.com>
2023-06-12 19:15:48 +0300

    Create ReminderModel.cs

---
+++ MyNotes/Models/ReminderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.Models;
+public class Reminder
+{
+    public string? ReminderHeader { get; set; }
+    public string? ReminderText { get; set; }
+    public string? DateTime { get; set; }
+    public string? Repeat { get; set; }
+}
+
