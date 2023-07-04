7e4d2429cf5f4892e235d64385880b7dc75153ba
ErenCanUtku <erencan.utku52@gmail.com>
2023-07-04 22:17:25 +0300

    Update RemindersPage.xaml.cs

--- MyNotes/Views/RemindersPage.xaml.cs
+++ MyNotes/Views/RemindersPage.xaml.cs
@@ -45,6 +45,7 @@ public sealed partial class RemindersPage : Page
     private static string noteName = string.Empty;
     public RemindersPage()
     {
+        //TODO:Add ReminderDetails page
         ViewModel = App.GetService<RemindersViewModel>();
         InitializeComponent();
         deleteFlyout.Text = "DeleteFlyout".GetLocalized();
